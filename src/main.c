f8bb62f5329938d0ca98db23588cc38268779afe
Maciej Sobkowski <maciejjo@maciejjo.pl>
2015-09-22 23:37:03 +0200

    Add sample matrices and multiply them

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "matrix.h"
 
 
@@ -9,16 +10,33 @@
 int main()
 {
 
+	srand(time(NULL));
+
+	int x = 10, y = 10;
+
 	int i;
+	struct matrix *matrix_a = NULL,
+		      *matrix_b = NULL,
+		      *matrix_c = NULL;
 
-	omp_set_num_threads(THREADS_NO);
-
-	printf("Hello travis \n");
-#pragma omp parallel
-	{
-		for(i = 0; i <= 5; i++)
-			printf("[%d] Hello World, i = %d\n", omp_get_thread_num(), i);
+	if(matrix_init(&matrix_a, x, y)) {
+		return EXIT_FAILURE;
 	}
+
+	if(matrix_init(&matrix_b, x, y)) {
+		return EXIT_FAILURE;
+	}
+
+	if(matrix_init(&matrix_c, x, y)) {
+		return EXIT_FAILURE;
+	}
+
+	matrix_populate_random(matrix_a);
+	matrix_populate_random(matrix_b);
+
+	matrix_multiply_jki(matrix_a, matrix_b, matrix_c);
+
+	matrix_print(matrix_c);
 
 	return EXIT_SUCCESS;
 

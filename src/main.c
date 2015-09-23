a01dcef6d6af182baa640229fa00192cd31b967b
Pawel Kusz <pawel.kusz@nokia.com>
2015-09-23 17:44:19 +0200

    Add function to check arguments, dynamic arguments
    Function check_arg checks that arguments pass from userspace are
    correct, in case one of arguments is incorrect, terminate the program
    and return EXIT_FAILURE.
    
    Size of matrix and number of thread are taken from userspace.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #define _GNU_SOURCE
 
-//#include <omp.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <getopt.h>
 #include "matrix.h"
 
 
-#define THREADS_NO 4
+
+
 
 struct timespec get_timespec_diff(struct timespec start, struct timespec end)
 {
@@ -22,20 +24,77 @@ struct timespec get_timespec_diff(struct timespec start, struct timespec end)
 	}
 	return temp;
 }
-
-int main()
+		
+void print_usage(FILE *stream, const char *program_name)
 {
-	printf("Hello world\n");
+	fprintf(stream, "**************************************"
+			"*********************************\n\n");
+	fprintf(stream,"Usage: \t  %s "
+	               "\t size "
+	               "\t threads_no \n",
+	               program_name);
+	fprintf(stream, "\n**************************************"
+			"*********************************\n");
 
-	srand(time(NULL));
+}
 
-	int x = 100, y = 100;
+int check_arg(int * param, char * arg)
+{
+	if ( 0 == (*param = atoi(arg))) 
+		return -1;
+	return 0;
+}	
 
+int main(int argc, char *argv[])
+{
+
+	int x,
+	    y,
+	    verbose = 0,
+	    no_threads;
+/*
+	const struct option long_options[] = {
+		{ "help",    0, NULL, 'h' },
+		{ "threads", 1, NULL, 't' },
+		{ "verbose", 0, NULL, 'v' ),
+		{ "size",    1, NULL, 's' },
+		{  NULL,     0, NULL,  0  }
+	};
+
+*/
 	struct timespec ts_start, ts_end, ts_diff;
 
 	struct matrix *matrix_a = NULL,
 		      *matrix_b = NULL,
 		      *matrix_c = NULL;
+
+	if (argc != 3 ) {
+		print_usage(stdout,argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if ( -1 == check_arg(&x,argv[1])) {
+		fprintf(stderr,"Size is not int or equal to 0\n");
+		return EXIT_FAILURE;
+	}
+
+	if ( -1 == check_arg(&y,argv[1])) {
+		fprintf(stderr,"Size is not int or equal to 0\n");
+		return EXIT_FAILURE;
+	}
+
+	if ( -1 == check_arg(&no_threads,argv[2])) {
+		fprintf(stderr,"Threads_no  is not int or equal to 0\n");
+		return EXIT_FAILURE;
+	}
+
+	if (verbose == 1 ) {
+		printf("Size x matrix is:%d\n",x);
+		printf("Size y matrix is:%d\n",y);
+		printf("The desired number of threads is:%d\n",no_threads);
+	}
+
+	srand(time(NULL));
 
 	if(matrix_init(&matrix_a, x, y)) {
 		return EXIT_FAILURE;
@@ -71,7 +130,6 @@ int main()
 	matrix_free(&matrix_a);
 	matrix_free(&matrix_b);
 	matrix_free(&matrix_c);
-
 	return EXIT_SUCCESS;
 
 }

8b156118983fb48a908c7836d462d5c2bef33d34
Maciej Sobkowski <maciejjo@maciejjo.pl>
2015-09-24 23:23:20 +0200

    Cleanup code

--- src/main.c
+++ src/main.c
@@ -30,18 +30,17 @@ void print_usage(FILE *stream, const char *program_name)
 			program_name);
 }
 
-int check_arg(int * param, char * arg)
+int check_arg(int *param, char *arg)
 {
 	if ( 0 == (*param = atoi(arg))) 
 		return -1;
 	return 0;
-}	
+}
 
 int main(int argc, char *argv[])
 {
 
 	int x,
-	    y,
 	    radius   =  0,
 	    func_idx =  0,
 	    no_threads;
@@ -54,23 +53,15 @@ int main(int argc, char *argv[])
 		matrix_multiply_6jki
 	};
 
-	void (*matrix_multiply)(struct matrix *a, struct matrix *b, struct matrix *c,int r);
-/*
-	const struct option long_options[] = {
-		{ "help",    0, NULL, 'h' },
-		{ "threads", 1, NULL, 't' },
-		{ "verbose", 0, NULL, 'v' ),
-		{ "size",    1, NULL, 's' },
-		{  NULL,     0, NULL,  0  }
-	};
+	void (*matrix_multiply)(struct matrix *a, struct matrix *b, struct matrix *c, int r);
 
-*/
 	struct timespec ts_start, ts_end, ts_diff;
 
 	struct matrix *matrix_a = NULL,
 		      *matrix_b = NULL,
 		      *matrix_c = NULL;
 
+	/* Validate parameters */
 
 	if (argc < 5 ) {
 		print_usage(stderr,argv[0]);
@@ -78,11 +69,6 @@ int main(int argc, char *argv[])
 	}
 
 	if ( -1 == check_arg(&x,argv[1])) {
-		fprintf(stderr,"Size is not int or equal to 0\n");
-		return EXIT_FAILURE;
-	}
-
-	if ( -1 == check_arg(&y,argv[1])) {
 		fprintf(stderr,"Size is not int or equal to 0\n");
 		return EXIT_FAILURE;
 	}
@@ -97,14 +83,17 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	if ( 0 == strncmp(argv[4],"ijk",3)) 
+	if ( 0 == strncmp(argv[4],"ijk",3)) {
 		func_idx = 0;
-
-	if ( 0 == strncmp(argv[4],"jki",3)) 
+	} else if ( 0 == strncmp(argv[4],"jki",3))  {
 		func_idx = 1;
-
-	if ( 0 == strncmp(argv[4],"6jki",4)) 
+	} else if ( 0 == strncmp(argv[4],"6jki",4)) {
 		func_idx = 2;
+	} else {
+		fprintf(stderr, "Wrong function name!\n");
+	};
+
+	/* Initialize program */
 
 	if(argc == 6) {
 
@@ -119,7 +108,7 @@ int main(int argc, char *argv[])
 
 	if (DEBUG_PRINT) {
 		printf("Size x matrix: %d\n",x);
-		printf("Size y matrix: %d\n",y);
+		printf("Size y matrix: %d\n",x);
 		printf("Threads no: %d\n",no_threads);
 		printf("Output file: %s\n",argv[5]);
 		printf("Function id: %d\n",func_idx);
@@ -129,27 +118,29 @@ int main(int argc, char *argv[])
 
 	srand(time(NULL));
 
-	if(matrix_init(&matrix_a, x, y)) {
+	if(matrix_init(&matrix_a, x, x)) {
 		return EXIT_FAILURE;
 	}
 
-	if(matrix_init(&matrix_b, x, y)) {
+	if(matrix_init(&matrix_b, x, x)) {
 		return EXIT_FAILURE;
 	}
 
-	if(matrix_init(&matrix_c, x, y)) {
+	if(matrix_init(&matrix_c, x, x)) {
 		return EXIT_FAILURE;
 	}
 
 	matrix_populate_random(matrix_a);
 	matrix_populate_random(matrix_b);
 
+	/* Perform measurement on computation */
+
 	if(clock_gettime(CLOCK_REALTIME, &ts_start) == -1) {
 		perror("clock gettime");
 		exit(EXIT_FAILURE);
 	}
 
-	matrix_multiply(matrix_a, matrix_b, matrix_c,radius);
+	matrix_multiply(matrix_a, matrix_b, matrix_c, radius);
 
 	if(clock_gettime(CLOCK_REALTIME, &ts_end) == -1) {
 		perror("clock gettime");
@@ -163,13 +154,7 @@ int main(int argc, char *argv[])
 
 	fprintf(output_file, "%d,%ld.%09ld\n", x, ts_diff.tv_sec, ts_diff.tv_nsec);
 
-	/*
-	n = write(fd,buff,strlen(buff));
-	if ( n != strlen(buff)) {
-		perror("write:");
-		return EXIT_FAILURE;
-	}
-	*/
+	/* Deinitialize */
 
 	matrix_free(&matrix_a);
 	matrix_free(&matrix_b);
